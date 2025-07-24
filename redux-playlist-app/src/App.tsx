b5d3ddbda07004d1b3a843f115fbf00bdd1e2c28
DebaKaran <karan.deba@gmail.com>
2025-07-24 21:18:19 +0530

    feat: add reset functionality to clear both songs and movies playlists - Added `reset` reducer to both songsSlice and moviesSlice - Exported as `resetSongs` and `resetMovies` respectively - Implemented Reset Both Playlists button in App.tsx - Dispatches both reset actions on click

--- redux-playlist-app/src/App.tsx
+++ redux-playlist-app/src/App.tsx
@@ -1,9 +1,16 @@
+import { useDispatch } from "react-redux";
 import MoviePlaylist from "./components/MoviePlaylist";
 import SongPlaylist from "./components/SongPlaylist";
 
+import { resetMovies, resetSongs, type AppDispatch } from "./store";
+
 export default function App() {
+  // Strongly typed dispatch
+  const dispatch = useDispatch<AppDispatch>();
+
   const handleResetClick = () => {
-    //
+    dispatch(resetMovies());
+    dispatch(resetSongs());
   };
 
   return (
