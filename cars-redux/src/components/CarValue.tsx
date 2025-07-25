7b801e501e02d466ee51282a838dbbb0ac2df85b
DebaKaran <karan.deba@gmail.com>
2025-07-25 13:49:43 +0530

    feat: add CarValue component to display total cost of filtered cars

--- cars-redux/src/components/CarValue.tsx
+++ cars-redux/src/components/CarValue.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import type { RootState } from "../store";
 
 const CarValue = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const totalCost = useSelector((state: RootState) => {
+    const { data, searchTerm } = state.cars;
 
-export default CarValue
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    let cost = 0;
+    for (const car of filteredCars) {
+      cost += car.cost;
+    }
+    return cost;
+  });
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
+};
+
+export default CarValue;
