4d5096f6c943cd3d81a68b698457b2d788964a4b
DebaKaran <karan.deba@gmail.com>
2025-07-25 12:39:32 +0530

    feat(car-list): implement car deletion using removeCar action from Redux store

--- cars-redux/src/components/CarList.tsx
+++ cars-redux/src/components/CarList.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeCar, type RootState } from "../store";
 
 const CarList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const cars = useSelector((state: RootState) => state.cars.data);
+  const dispatch = useDispatch();
+  const handleCarDelete = (carId: string) => {
+    dispatch(removeCar(carId));
+  };
 
-export default CarList
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car.id)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+  console.log(cars);
+  return <div>{renderedCars}</div>;
+};
+
+export default CarList;
