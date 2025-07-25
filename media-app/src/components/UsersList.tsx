5fab0e119bcdd98d42b11bcaf4af51458cc001cb
DebaKaran <karan.deba@gmail.com>
2025-07-25 23:57:37 +0530

    fix(users): trigger fetchUsers thunk in useEffect and log loading/error/data state

--- media-app/src/components/UsersList.tsx
+++ media-app/src/components/UsersList.tsx
@@ -1,13 +1,26 @@
 import { useEffect } from "react";
 import { fetchUsers } from "../store";
-import { useAppDispatch } from "../store/hooks";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
 
 const UsersList = () => {
   const dispath = useAppDispatch(); //properly typed dispatch
+  const { isLoading, data, error } = useAppSelector((state) => {
+    return state.users;
+  });
+
   useEffect(() => {
     dispath(fetchUsers());
   }, [dispath]);
-  return <div>Users List</div>;
+  console.log("Loading:", isLoading, "Error:", error, "Data:", data);
+
+  if (isLoading) {
+    return <div>Loading ....</div>;
+  }
+
+  if (error) {
+    return <div>Error Fetching data ...</div>;
+  }
+  return <div>{data.length}</div>;
 };
 
 export default UsersList;
